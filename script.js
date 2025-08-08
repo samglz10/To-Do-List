941e50d693769c4fb59fe2e35e7c38bcea521100
Sammy G <samglz10@gmail.com>
2025-08-07 21:26:56 -0400

    updated

--- script.js
+++ script.js
@@ -2,40 +2,75 @@
 const submitTodo = document.getElementById('addBtn');
 const taskLists = document.getElementById('taskLists');
 const inputField = document.getElementById('inputField');
-const githubLink = document.getElementById('github-link')
+const githubLink = document.getElementById('github-link');
+const defaultTask = document.getElementById('defaultTask');
+const allTasks = [];
 
-submitTodo.addEventListener('click', function(){
-    //creates a new li tag
+
+function uniqueKey(number) {
+    let randomNumber = Math.floor(Math.random() * number) + 1;
+    let timestamp = Date.now();
+    let uniqueKey = randomNumber + '-' + timestamp;
+
+    console.log(uniqueKey);
+    return uniqueKey;
+};
+
+
+submitTodo.addEventListener('click', function () {
+
+    let newTask = {
+        "key": uniqueKey(100),
+        priority: "",
+        description: "",
+        creationDate: Date.now(),
+        dueDate: "",
+        completed: false,
+        deleted: false,
+        completionDate: "",
+    };
+
+
+
+    console.log(newTask);
+    //console.log(uniqueKey(10));
+
+
+
 
     const newTodo = document.createElement('li');
     newTodo.innerText = inputField.value;
-    if (inputField.value ===''){
-        alert('Please enter a Task')
+
+    
+    if (inputField.value === '') {
+        alert('Please enter a Task');
     } else {
-    newTodo.classList.add('newTodoStyle')
-    taskLists.appendChild(newTodo);
-    inputField.value='';
+        newTodo.classList.add('newTodoStyle')
+        taskLists.appendChild(newTodo);
+        allTasks.push(newTask);
+        console.log(allTasks);
+        inputField.value = "";
 
-    newTodo.addEventListener('click', function(){
-        newTodo.classList.toggle("CompletedList")
-    })
+        newTodo.addEventListener('click', function () {
+            newTodo.classList.toggle("CompletedList")
+        })
 
-    newTodo.addEventListener('dblclick', function(){
-        taskLists.removeChild(newTodo);
-    })
+        newTodo.addEventListener('dblclick', function () {
+            taskLists.removeChild(newTodo);
+        });
+
+
     }
 
-    //appends the child to field
-    
-     
 })
 
+defaultTask.addEventListener('click', () => {
+    console.log('clicked')
+    defaultTask.classList.toggle("CompletedList");
+    console.log(defaultTask.classList)
+})
 
-
-
-
-
-
-
-
+defaultTask.addEventListener('dblclick', function () {
+    defaultTask.remove(defaultTask);
+});
 
